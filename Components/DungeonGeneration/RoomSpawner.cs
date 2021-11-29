94cecba61bcab11eddb9acbda70bb1c10e4cf55e
franmajka <rudik2703@gmail.com>
2021-11-29 20:51:02 +0200

    Implement room geeration

---
+++ Components/DungeonGeneration/RoomSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public int openingDirection;
+    // 1 --> bottom door
+    // 2 --> top door
+    // 3 --> left door
+    // 4 --> right door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    public float waitTime = 4f;
+    private void Start()
+    {
+        Destroy(gameObject, waitTime);
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    private void Spawn()
+    {
+        if (!spawned)
+        {
+            if (openingDirection == 1)
+            {
+                // spawn a rooom with a bottom door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                // spawn a rooom with a top door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                // spawn a rooom with a left door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                // spawn a rooom with a right door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+        {
+            if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                //spawn walls blocking off any opening
+                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+
+
+}
---
+++ Components/DungeonGeneration/RoomTemplates.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+    public GameObject[] bottomRooms;
+    public GameObject[] topRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] leftRooms;
+    public GameObject closedRoom;
+    public List<GameObject> rooms;
+
+    public float waitTime;
+    private bool spawnedPilon;
+    public GameObject pilon;
+
+    private void Update()
+    {
+        if (waitTime <= 0 && !spawnedPilon)
+        {
+            for (int i = 0; i < rooms.Count; i++)
+            {
+                if (i == rooms.Count - 1)
+                {
+                    Instantiate(pilon, rooms[i].transform.position, Quaternion.identity);
+                    spawnedPilon = true;
+                }
+            }
+        } else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
+}
